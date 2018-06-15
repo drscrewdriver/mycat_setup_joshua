a44ae02b51888108f0cb36aaab05a3c23c064a78
drscrewdriver <597093681@qq.com>
2018-06-16 01:24:28 +0800

    0615-1718-save

--- setup_mysql.sh
+++ setup_mysql.sh
@@ -29,22 +29,159 @@ if [ $lost_some_depends -eq 0  ] ;then
     echo start setup mysqldata dirs
 fi
 
-mkdir -p /mysqldata/3306/data/
+
 chown mysql.mysql -R /mysqldata/
 #是否应该先赋值目录名字《--datadir 以及 dir
-#复制或生成my.cnf
-SOCKET=
-PATHERRORLOG=
+
 DATADIR=/mysqldata/$PORT/data/
+mkdir -p $DATADIR
+
+
+#复制或生成my.cnf
+CMDPATH=/application/mysql/bin/
+DATADIRBASE=/mysqldata/$PORT/
+SOCKET=/mysqldata/$PORT/mysql.sock
+PATHERRORLOG=/mysqldata/$PORT/mysqlerror-$PORT.log
 DEFAULTSFLIE=/mysqldata/$PORT/my.cnf
+DBID=10
+MAXCON=4096
 #设置 数据库主库 预设主库id
 #确认主库binlog功能打开
-
+cat <<EOF >$DEFAULTSFLIE
+[client]
+port            = $PORT
+socket          = $SOCKET
+[mysql]
+prompt="(\\u@\\h) [\\d]>\\_\\r:\\m:\\s>"
+EOF
+cat <<EOF >>$DEFAULTSFLIE
+[mysqld]
+datadir=$DATADIR
+user    = mysql
+port    = $PORT
+basedir = /application/mysql
+open_files_limit = 65535
+socket=$SOCKET
+#default utf-8
+default-storage-engine = innodb
+collation-server = utf8_general_ci
+character-set-server = utf8
+innodb_file_per_table
+back_log = 600
+max_connections = $MAXCON
+max_connect_errors = 3000
+max_allowed_packet = 16M
+sort_buffer_size = 4M
+join_buffer_size = 4M
+thread_cache_size = 1000
+query_cache_size = 16M
+query_cache_limit = 64M
+query_cache_min_res_unit = 2k
+thread_stack = 192K
+tmp_table_size = 4M
+max_heap_table_size = 4M
+slow-query-log=1
+long_query_time = 1
+slow-query-log-file=/mysqldata/3306/slow.log
+server-id=$DBID
+log-bin = mysql-bin$PORT
+binlog_cache_size = 4M
+max_binlog_cache_size = 8M
+max_binlog_size = 2M
+expire_logs_days = 7
+key_buffer_size = 32M
+read_buffer_size = 16M
+read_rnd_buffer_size = 16M
+bulk_insert_buffer_size = 16M
+symbolic-links=0
+innodb_buffer_pool_size = 32M
+#innodb_data_file_path = ibdata1:128M:autoextend
+innodb_file_io_threads = 8
+innodb_thread_concurrency = 8
+innodb_flush_log_at_trx_commit = 2
+innodb_log_buffer_size = 4M
+innodb_log_file_size = 8M
+innodb_log_files_in_group = 3
+innodb_max_dirty_pages_pct = 90
+innodb_lock_wait_timeout = 120
+# Recommended in standard MySQL setup
+sql_mode=NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES
+EOF
+cat <<EOF >>$DEFAULTSFLIE
+log-error=$PATHERRORLOG
+pid-file=$DATADIRBASE/${PORT}.pid
+EOF
 #初始化
 /application/mysql/scripts/mysql_install_db --user=mysql --basedir=/application/mysql/ --datadir=$DATADIR --defaults-file=$DEFAULTSFLIE
 ###!!!!!重要需要touch出errorlog，否则不能启动
-
+touch $PATHERRORLOG
 #复制启动脚本目录 或/etc/init.d/ mysql为prefix 后跟端口号
+cat <<EOF >> /etc/init.d/mysql-$PORT
+#!/bin/sh
+################################################
+#this scripts is created by joshua at 2017-06-09
+#joshau QQ:597093681
+#blog:http://www.drscrewdriver.com
+################################################
+
+#init
+port=${PORT}
+mysql_user=root
+mysql_pwd=""
+CmdPath=${CMDPATH}
+mysql_sock=${SOCKET}
+#startup function
+function_start_mysql()
+{
+    if [ ! -e "${SOCKET}" ];then
+      printf "Starting MySQL...\n"
+#      /bin/sh "${CMDPATH}"/mysqld_safe --defaults-file=/medias/mysqldata/mysqldata/my.cnf 2>&1 > /dev/null &
+     /bin/sh "${CMDPATH}"/mysqld_safe --defaults-file=/mysqldata/3306/my.cnf 2>&1 > /dev/null &
+    else
+      printf "MySQL is running...\n"
+      exit
+    fi
+}
+
+#stop function
+function_stop_mysql()
+{
+    if [ ! -e "${SOCKET}" ];then
+       printf "MySQL is stopped...\n"
+       exit
+    else
+       printf "Stoping MySQL...\n"
+       #${CMDPATH}/mysqladmin -uroot -p'${mysql_pwd}' -S /medias/mysqldata/mysql/mysql.sock shutdown
+	   ${CMDPATH}/mysqladmin -uroot -S /mysqldata/3306/mysql.sock shutdown
+   fi
+}
+
+#restart function
+function_restart_mysql()
+{
+    printf "Restarting MySQL...\n"
+    function_stop_mysql
+    sleep 2
+    function_start_mysql
+}
+
+case $1 in
+start)
+    function_start_mysql
+;;
+stop)
+    function_stop_mysql
+;;
+restart)
+    function_restart_mysql
+;;
+*)
+    printf "Usage: '$0' {start|stop|restart}\n"
+esac
+
+
+EOF
+
 #启动mysql实例
 #import
 #reconfig read write count
