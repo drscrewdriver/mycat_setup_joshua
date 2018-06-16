20604c3fb4d912b7e7a6317b2c97a16b67e3d8e5
drscrewdriver <597093681@qq.com>
2018-06-17 00:21:15 +0800

    0616-night

--- setup_mysql.sh
+++ setup_mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #yum yum源预设
 #pre
-wget -O /etc/yum.repos.d/Centos-6.repo http://mirrors.aliyun.com/repo/Centos-6.repo
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-6.repo
 wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-6.repo
 yum -y install wget vim telnet numactl libaio nmap wget vim 
 #mkdir
