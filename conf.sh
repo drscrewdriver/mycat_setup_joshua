8e0771b4b308f1b1d47c8f819b697e6927720897
root <root@mysql-master.(none)>
2018-06-19 19:52:36 +0800

    0619-noon2

---
+++ conf.sh
@@ -0,0 +1,11 @@
+PORT=3306
+preid=1000
+DATADIR=/mysqldata/${PORT}/data/
+CMDPATH=/application/mysql/bin/
+DATADIRBASE=/mysqldata/${PORT}/
+SOCKET=/mysqldata/${PORT}/mysql.sock
+PATHERRORLOG=/mysqldata/${PORT}/mysqlerror-${PORT}.log
+DEFAULTSFLIE=/mysqldata/${PORT}/my.cnf
+DBID=10
+MAXCON=4096
+MYSQL_MSCRIPT=/etc/init.d/mysql-${PORT}
